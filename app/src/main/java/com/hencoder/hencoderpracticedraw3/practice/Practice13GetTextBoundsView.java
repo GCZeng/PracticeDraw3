df5e6835e79ae415c84ab37b5f8f83480759c9d0
ZGC <395804951@qq.com>
2017-07-25 14:29:20 +0800

    1. 完成练习；

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -50,11 +51,23 @@ public class Practice13GetTextBoundsView extends View {
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        Rect bound = new Rect();
+        paint2.getTextBounds(text1, 0, text1.length(), bound);
+        canvas.drawText(text1, 100, middle - (bound.bottom + bound.top) / 2, paint2);
+
+        paint2.getTextBounds(text2, 0, text2.length(), bound);
+        canvas.drawText(text2, 200, middle - (bound.bottom + bound.top) / 2, paint2);
+
+        paint2.getTextBounds(text3, 0, text3.length(), bound);
+        canvas.drawText(text3, 300, middle - (bound.bottom + bound.top) / 2, paint2);
+
+        paint2.getTextBounds(text4, 0, text4.length(), bound);
+        canvas.drawText(text4, 400, middle - (bound.bottom + bound.top) / 2, paint2);
+
+        paint2.getTextBounds(text5, 0, text5.length(), bound);
+        canvas.drawText(text5, 500, middle - (bound.bottom + bound.top) / 2, paint2);
+
+        paint2.getTextBounds(text6, 0, text6.length(), bound);
+        canvas.drawText(text6, 600, middle - (bound.bottom + bound.top) / 2, paint2);
     }
 }
